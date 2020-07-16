0d0fd98dbef10f4b03e575ebcdf58dc0089dd820
julianamparo <juliana.amparo90@gmail.com>
2020-07-15 22:24:13 -0300

    Começando no js

---
+++ teste01.js
@@ -0,0 +1,8 @@
+function criarAnimal(tipoDeAnimal, nomeDoAnimal, porte){
+    return tipoDeAnimal = tipoDeAnimal,
+           nomeDoAnimal = nomeDoAnimal,
+           porte = porte;
+}
+
+const animalNovo = criarAnimal("Cachorro","Frida", "Gorda");
+console.log(animalNovo);
